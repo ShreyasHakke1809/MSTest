a5ceca45d2626ed0554d3fa4069687acc4dedab7
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-10-22 18:57:04 +0530

    adding mstest practicr problem program

---
+++ First/Program.cs
@@ -0,0 +1,44 @@
+﻿using MSTestPracticeProgram;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("1.Duplicate Number\n2.Swap Number\n3.Exit");
+        bool loopAgain = true;
+        while (loopAgain)
+        {
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    PurchaseList list = new PurchaseList();
+                    list.List();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the words separated by a space:");
+                    string[] words = Console.ReadLine().Split(' ');
+
+                    if (words.Length != 2)
+                    {
+                        Console.WriteLine("Invalid input. Please enter two words separated by a space.");
+                        return;
+                    }
+                    SwapConnection swapConnection = new SwapConnection();
+                    bool canRearrange = swapConnection.Rearrange(words[0], words[1]);
+
+                    Console.WriteLine(canRearrange ? "YES" : "NO");
+                    break;
+                    break;
+                case 3:
+                    Environment.Exit(0);
+                    break;
+                case 4:
+                    loopAgain = false;
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ First/PurchaseList.cs
@@ -0,0 +1,66 @@
+﻿namespace MSTestPracticeProgram
+{
+    public class PurchaseList
+    {
+        public int[] ListA()
+        {
+            int[] list = { 7, 5, 4, 7, 94, 1, 11 };
+            int len = list.Length;
+            int soldForOnce = 0;
+            int soldMoreThanOnce = 0;
+            bool flag = false;
+            Dictionary<int, int> frequencyOfPrice = new Dictionary<int, int>();
+            foreach (int i in list)
+            {
+                if (frequencyOfPrice.ContainsKey(i))
+                {
+                    frequencyOfPrice[i]++;
+                }
+                else
+                {
+                    frequencyOfPrice[i] = 1;
+                }
+            }
+            foreach (int Key in frequencyOfPrice.Keys)
+            {
+                if (frequencyOfPrice[Key] == 1)
+                {
+                    soldForOnce = Key;
+                    break;
+                }
+            }
+            foreach (int Key in frequencyOfPrice.Keys)
+            {
+                if (frequencyOfPrice[Key] > 1)
+                {
+                    soldMoreThanOnce = Key;
+                    break;
+                }
+            }
+            if (list.Length == 0)
+            {
+                Console.WriteLine("\nNone");
+            }
+            else
+            {
+                bool isRepeating = false;
+                foreach (int Key in frequencyOfPrice.Keys)
+                {
+                    if (frequencyOfPrice[Key] != 1)
+                    {
+                        isRepeating = true;
+                        break;
+                    }
+                }
+                Console.WriteLine("\n" + soldForOnce);
+                if (!isRepeating)
+                    Console.WriteLine("None");
+                else
+                    Console.WriteLine(soldMoreThanOnce);
+            }
+            int[] arrReturn = { soldForOnce, soldMoreThanOnce };
+            return arrReturn;
+        }
+    }
+}
+
